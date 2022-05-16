29b2b35fc08f7fb862ffefb7f9a5811334f7d4b5
russian-achilles <o232079t@mail.kyutech.jp>
2022-05-16 19:14:57 +0900

    Add CommentForm Class

---
+++ src/main/java/com/example/trainingspring/application/form/CommentForm.java
@@ -0,0 +1,10 @@
+package com.example.trainingspring.application.form;
+
+import lombok.Data;
+
+@Data
+public class CommentForm {
+    private String name;
+    private String mailAddress;
+    private String comment;
+}
