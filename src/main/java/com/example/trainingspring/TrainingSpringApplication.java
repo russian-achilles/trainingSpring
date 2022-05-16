8afc6bf09d0f4651983becebe953614f898b11a9
russian-achilles <o232079t@mail.kyutech.jp>
2022-05-16 13:34:58 +0900

    initial commit

---
+++ src/main/java/com/example/trainingspring/TrainingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.trainingspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trainingspring/TrainingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trainingspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
