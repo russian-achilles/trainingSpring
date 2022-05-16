cd9f93405e6286fc18ad259aad674ff453b39b91
russian-achilles <o232079t@mail.kyutech.jp>
2022-05-16 21:01:08 +0900

    Make validate comment form

--- src/main/java/com/example/trainingspring/application/form/CommentForm.java
+++ src/main/java/com/example/trainingspring/application/form/CommentForm.java
@@ -1,10 +1,23 @@
 package com.example.trainingspring.application.form;
 
+import javax.validation.constraints.Email;
+
+import org.hibernate.validator.constraints.Length;
+
+import groovyjarjarantlr4.v4.runtime.misc.NotNull;
+import groovyjarjarantlr4.v4.runtime.misc.Nullable;
 import lombok.Data;
 
 @Data
 public class CommentForm {
+    @Nullable
+    @Length(max = 20)
     private String name;
+    @Nullable
+    @Email
+    @Length(max = 100)
     private String mailAddress;
+    @NotNull
+    @Length(min = 1, max = 400)
     private String comment;
 }
