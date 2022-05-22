3b4f39e441a69154e34d8c37011695ec5108e2c3
russian-achilles <o232079t@mail.kyutech.jp>
2022-05-22 15:51:46 +0900

    Add omikuji function

--- src/main/java/com/example/trainingspring/domain/model/UserComment.java
+++ src/main/java/com/example/trainingspring/domain/model/UserComment.java
@@ -1,5 +1,7 @@
 package com.example.trainingspring.domain.model;
 
+import java.util.Random;
+
 import com.example.trainingspring.domain.type.Comment;
 import com.example.trainingspring.domain.type.MailAddress;
 import com.example.trainingspring.domain.type.Name;
@@ -17,5 +19,27 @@ public class UserComment {
 
     public static UserComment from(String name, String mailAddress, String comment) {
         return new UserComment(Name.from(name), MailAddress.from(mailAddress), Comment.from(comment));
+    }
+
+    /**
+     * 名前が[!omikuji]の場合おみくじの結果を返す。
+     * そうでない場合は名前を返す。
+     * @return Name
+     */
+    public Name getName() {
+        if (!name.equals("!omikuji")) {
+            return name;
+        }
+
+        int random = new Random().nextInt(3);
+
+        switch (random) {
+            case 0:
+                return Name.from("大吉");
+            case 1:
+                return Name.from("中吉");
+            default:
+                return Name.from("小吉");
+        }
     }
 }
