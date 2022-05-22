9a13a64d460de3b4792f84610df2742eeaac8a3a
russian-achilles <o232079t@mail.kyutech.jp>
2022-05-22 21:01:00 +0900

    Use repository

--- src/main/java/com/example/trainingspring/infrastructure/datasource/UserCommentDatasource.java
+++ src/main/java/com/example/trainingspring/infrastructure/datasource/UserCommentDatasource.java
@@ -1,12 +1,20 @@
 package com.example.trainingspring.infrastructure.datasource;
 
+import com.example.trainingspring.application.form.dto.UserCommentDto;
 import com.example.trainingspring.domain.model.UserComment;
 import com.example.trainingspring.domain.model.UserCommentRepository;
 
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Repository
 public class UserCommentDatasource implements UserCommentRepository {
+    private final UserCommentMapper mapper;
+
     @Override
     public void save(UserComment userComment) {
-        // TODO Auto-generated method stub
-        
+        mapper.Insert(UserCommentDto.from(userComment));
     }
 }
