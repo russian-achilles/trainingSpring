a80589d318a3271e2933ac0853c3fcf418803436
russian-achilles <o232079t@mail.kyutech.jp>
2022-05-22 20:48:08 +0900

    Add repository

---
+++ src/main/java/com/example/trainingspring/domain/model/UserCommentRepository.java
@@ -0,0 +1,5 @@
+package com.example.trainingspring.domain.model;
+
+public interface UserCommentRepository {
+    void save(UserComment userComment);
+}
---
+++ src/main/java/com/example/trainingspring/infrastructure/datasource/UserCommentDatasource.java
@@ -0,0 +1,12 @@
+package com.example.trainingspring.infrastructure.datasource;
+
+import com.example.trainingspring.domain.model.UserComment;
+import com.example.trainingspring.domain.model.UserCommentRepository;
+
+public class UserCommentDatasource implements UserCommentRepository {
+    @Override
+    public void save(UserComment userComment) {
+        // TODO Auto-generated method stub
+        
+    }
+}
